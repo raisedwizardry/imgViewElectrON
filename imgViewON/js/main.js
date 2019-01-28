e22db15cee1a8e24aa5a990a986a3eb91d547199
raisedwizardry <lifebloodasmr@gmail.com>
2019-01-28 12:16:05 -0500

    created SetUpWindow function, clean up, and reorganization
    Signed-off-by: raisedwizardry <lifebloodasmr@gmail.com>

--- imgViewON/js/main.js
+++ imgViewON/js/main.js
@@ -8,74 +8,49 @@ let windowObject = {
 	transparent: true
 };
 
-let args=nw.App.argv;
+let fileArguments=nw.App.argv;
 nw.Window.open('../index.html', windowObject, function(win) {
-	//win.showDevTools();
-	let filePath = FindFilePath(args);
-	filePath = RemoveExtraQuotes(filePath);
-	ValidateFilePath(filePath);
-	WriteToJson(filePath);
-	AddRightClickClosing(win);
+	SetUpWindow(fileArguments, win);
 });
 
 nw.App.on('open', function(cmdline) {
 	let fileArguments = SubsequentFilePath(cmdline);
 	nw.Window.open('../index.html', windowObject, function(win) {
-		//win.showDevTools();
-		let filePath = FindFilePath(fileArguments);
-		filePath = RemoveExtraQuotes(filePath);
-		ValidateFilePath(filePath);
-		WriteToJson(filePath);
-		AddRightClickClosing(win);
+		SetUpWindow(fileArguments, win);
 	});
 });
 
-function AddRightClickClosing(win) {
-	win.on('loaded', function() {
-		var doc = win.window.document;
-		doc.getElementById("dadresize").addEventListener("mouseup", function(event) { 
-			if (event.which === 3) {//right mouse
-				win.close(); 
-			}
-			console.log("click");
-		});
-	});
+function SetUpWindow(fileArguments, win) {
+	//win.showDevTools();
+	let filePath = FindFilePath(fileArguments, win);
+	filePath = RemoveExtraQuotes(filePath);
+	ValidateFilePath(filePath, win);
+	WriteToJson(filePath);
+	AddRightClickClosing(win);
 }
 
-function WriteToJson(path) {
-	let data = {filePath: path};
-	let json = JSON.stringify(data, null, 2);
-	fs.writeFileSync('imgFilePath.json', json);
-}
 
-function FindFilePath(arg) {
-	let arglen=args.length;
-	let imgpath;
+function FindFilePath(arg, windowObject) {
+	let arglen=arg.length;
+	let imgPath;
 	if (arglen === 1) {
-		imgpath = arg[0];
-		return imgpath;
+		imgPath = arg[0];
+		return imgPath;
 	}
 	else if (arglen > 1) {
-		imgpath = arg[0];
+		imgPath = arg[0];
 		alert("Error: One image at a time");
-		return imgpath;
+		return imgPath;
 	}
 	else if (arglen === 0) {
-		imgpath = "../img/sizing.png";
+		imgPath = "../img/sizing.png";
 		alert("Error: No image to display");
-		return imgpath;
+		return imgPath;
 	}
 	else {
 		alert("Error: Could not access image");
-		win.close();
+		windowObject.close();
 	}
-}
-
-function SubsequentFilePath(args) {
-	let fileArguments = /(.*)--original-process-start-time\=(\d+)(.*)/.exec(args).pop().split(' ');
-	fileArguments.shift();
-	fileArguments.shift();
-	return fileArguments;
 }
 
 function RemoveExtraQuotes(imgPath) {
@@ -87,10 +62,35 @@ function RemoveExtraQuotes(imgPath) {
 	}
 }
 
-function ValidateFilePath(imgPath) {
+function ValidateFilePath(imgPath, windowObject) {
 	if ((/\.(gif|jpg|jpeg|jpe|jif|jfi|jfif|webp|bmp|svg|svgz|png)$/i).test(imgPath)) {}
 	else {
 		alert("Error: File format not supported");
-		win.close();
+		windowObject.close();
 	}
+}
+
+function WriteToJson(imgPath) {
+	let data = {filePath: imgPath};
+	let json = JSON.stringify(data, null, 2);
+	fs.writeFileSync('imgFilePath.json', json);
+}
+
+function AddRightClickClosing(windowObject) {
+	windowObject.on('loaded', function() {
+		var doc = windowObject.window.document;
+		doc.getElementById("dadresize").addEventListener("mouseup", function(event) { 
+			if (event.which === 3) {//right mouse
+				windowObject.close(); 
+			}
+			console.log("click");
+		});
+	});
+}
+
+function SubsequentFilePath(arg) {
+	let fileArguments = /(.*)--original-process-start-time\=(\d+)(.*)/.exec(arg).pop().split(' ');
+	fileArguments.shift();
+	fileArguments.shift();
+	return fileArguments;
 }
