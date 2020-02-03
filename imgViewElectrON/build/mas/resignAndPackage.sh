c21cd95ef7df40d58a3c9cae0b3e8898b47c878d
raisedwizardry <lifebloodasmr@gmail.com>
2020-02-03 13:06:31 -0500

    add extra files for packaging mac app for mac store

---
+++ imgViewElectrON/build/mas/resignAndPackage.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+# Name of your app.
+APP="imgViewOn"
+# The path of your app to sign.
+APP_PATH="dist/mas/imgViewOn.app"
+# The path to the location you want to put the signed package.
+RESULT_PATH="dist/mas/$APP-mac_store.pkg"
+# The name of certificates you requested.
+APP_KEY="3rd Party Mac Developer Application: Eli Davis (2NX7NWH6BJ)"
+INSTALLER_KEY="3rd Party Mac Developer Installer: Eli Davis (2NX7NWH6BJ)"
+# The path of your plist files.
+PARENT_PLIST="build/mas/entitlements.mas.plist"
+CHILD_PLIST="build/mas/entitlements.mas.inherit.plist"
+LOGINHELPER_PLIST="build/mas/entitlements.mas.loginhelper.plist"
+FRAMEWORKS_PATH="$APP_PATH/Contents/Frameworks"
+
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper (GPU).app/Contents/MacOS/$APP Helper (GPU)"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper (Renderer).app/Contents/MacOS/$APP Helper (Renderer)"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper (Plugin).app/Contents/MacOS/$APP Helper (Plugin)"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Electron Framework"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Libraries/libffmpeg.dylib"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Libraries/libffmpeg.dylib"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper.app/Contents/MacOS/$APP Helper"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper.app/"
+codesign -s "$APP_KEY" -f --entitlements "$LOGINHELPER_PLIST" "$APP_PATH/Contents/Library/LoginItems/$APP Login Helper.app/Contents/MacOS/$APP Login Helper"
+codesign -s "$APP_KEY" -f --entitlements "$LOGINHELPER_PLIST" "$APP_PATH/Contents/Library/LoginItems/$APP Login Helper.app/"
+codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$APP_PATH/Contents/MacOS/$APP"
+codesign -s "$APP_KEY" -f --entitlements "$PARENT_PLIST" "$APP_PATH"
+productbuild --component "$APP_PATH" /Applications --sign "$INSTALLER_KEY" "$RESULT_PATH"
